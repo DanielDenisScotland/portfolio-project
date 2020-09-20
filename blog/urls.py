70335c45289a42324e893cd448ca58c14a39e8d7
ddenisiuk <ddenisiuk.sc@gmail.com>
2020-09-20 17:40:21 +0100

    Blog functionality and polish

---
+++ blog/urls.py
@@ -0,0 +1,8 @@
+from os import name
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.allblogs, name='allblogs'),
+    path('<int:blog_id>/', views.detail, name='detail'),
+]
